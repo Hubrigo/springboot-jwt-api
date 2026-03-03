36aa87ba8cffa6cb0beb9923d23b877e39efe893
Hugo Salcedo <hugofebrero1@hotmail.com>
2026-03-03 01:52:32 -0500

    firts commit

---
+++ src/main/java/com/hugo/springbootjwtapi/SpringbootjwtapiApplication.java
@@ -0,0 +1,13 @@
+package com.hugo.springbootjwtapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootjwtapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootjwtapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hugo/springbootjwtapi/SpringbootjwtapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hugo.springbootjwtapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootjwtapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
